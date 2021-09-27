1b270a9da1e4782b3e13ca50097506f3bb8574ff
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2021-09-28 00:36:34 +0530

    GraphQL Mutation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import { LessonModule } from './lesson/lesson.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
 })
 export class AppModule {}
---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,27 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+import { start } from 'repl';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+  @Query((returns) => LessonType)
+  lesson() {
+    return {
+      id: 'dsfdg',
+      name: 'dsds',
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
+  }
+}
