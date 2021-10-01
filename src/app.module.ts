a4fa2a21197bcb3c2f09592c01144ee74dc2d5a8
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2021-10-01 10:13:57 +0530

    module created: student

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
+import { StudentModule } from './student/student.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -16,6 +17,7 @@ import { LessonModule } from './lesson/lesson.module';
       autoSchemaFile: true,
     }),
     LessonModule,
+    StudentModule,
   ],
 })
 export class AppModule {}
